e83f167fd7faed6bcba926f45029f1c05d5f13cd
Leandro Santana <96756735+Leandro-Santana013@users.noreply.github.com>
2025-03-17 04:44:23 -0300

    exEnergia

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+double custoestimado = 0, consumoEnergia = 0, tempoUsoDiario = 0;
+
+Console.Write("Digite consumo de energia do seu equipamento (em kWh/mês): "); 
+consumoEnergia = double.Parse(Console.ReadLine());
+
+Console.Write("Tempo de uso diário (h): ");
+tempoUsoDiario = double.Parse(Console.ReadLine());
+
+custoestimado = consumoEnergia * tempoUsoDiario *  0.62258;
+
+Console.WriteLine($"O custo estimado de energia é: {custoestimado:C2}");
